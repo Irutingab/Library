a9fc9ea7def878daa02b9cf3dc2115cf561117f3
Irutingab <raissairutingabo@gmail.com>
2025-01-29 15:55:11 +0200

    add data to an excel sheet, and run the program from one file

---
+++ main.py
@@ -0,0 +1,10 @@
+from loader import LibraryManagement
+
+def main():
+    # Save data
+    manager = LibraryManagement()
+    manager.save_data_to_sheet()
+
+
+if __name__ == "__main__":
+    main()
