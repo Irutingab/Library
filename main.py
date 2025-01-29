2b42fba74edba3e219fafc101ae09e7d7de996f5
Irutingab <raissairutingabo@gmail.com>
2025-01-29 16:05:17 +0200

    modify excel sheets, and run the entire project from one file

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from loader import LibraryManagement
+from utilities.loader import LibraryManagement
+from utilities.user_edit import AddNewRowsandColumns
+from utilities.Library_updater import UpdateSheet
 
 def main():
     # Save data
     manager = LibraryManagement()
     manager.save_data_to_sheet()
+    
+    # Add new columns
+    appender = AddNewRowsandColumns()
+    appender.NewrowsAndcolumns()
+    
+    # Update rows
+    updater = UpdateSheet()
+    updater.SheetName()
 
 
 if __name__ == "__main__":
---
+++ utilities/Library_updater.py
@@ -0,0 +1,25 @@
+from openpyxl import load_workbook
+import os
+import openpyxl
+
+class UpdateSheet:
+    def __init__(self, file_name="./storage/Librarie_Saint_Laurent.xlsx"):
+        self.file_name = file_name
+        self.workbook = None
+        self.worksheet = None
+        
+    def SheetName(self):
+        if os.path.exists(self.file_name):
+                # Load the workbook
+            self.workbook = load_workbook(self.file_name)
+            self.worksheet = self.workbook.active
+                
+                # Update cell value
+            self.worksheet["C8"] = "2005"
+                
+                # Save changes using the instance variable
+            self.workbook.save(self.file_name)
+            print(f"Cell updated successfully in '{self.file_name}'")
+        else:
+            print(f"Error: File '{self.file_name}' not found")
+                
